c856c20378ab31ebb4a8ba389a6db444393559fe
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-02-06 20:08:15 +0530

    Day 12 of BD Journey started

---
+++ DAY-97/src/app.js
@@ -0,0 +1,10 @@
+
+const express = require("express")
+const app = express()
+
+
+//middleware
+app.use(express.json())
+
+
+module.exports = app
---
+++ DAY-97/src/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    password:String,
+    email:String
+
+})
+
+const userModel = mongoose.model("user" , userSchema)
+
+module.exports = userModel
