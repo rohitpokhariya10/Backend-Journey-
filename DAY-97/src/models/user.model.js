70798e3f91bd2f9de391d80d05c9749a3c0ee985
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-02-08 15:44:21 +0530

    day 12 of BD Journey completed

--- DAY-97/src/models/user.model.js
+++ DAY-97/src/models/user.model.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     name:String,
     password:String,
-    email:String
+    email:{
+        type:String,
+        unique:["true" , "User already exist with this emaillllll..."]
+    }
 
 })
 
